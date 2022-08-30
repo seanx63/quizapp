7b0ca763f12c5cb962999581d4bb5d7d54d8bb33
Sean Gates <seanx63@gmail.com>
2022-08-30 14:06:01 -0700

    implement CRUD features with three entities

--- src/main/java/com/genspark/quizapp/QuizappApplication.java
+++ src/main/java/com/genspark/quizapp/QuizappApplication.java
@@ -1,13 +1,30 @@
 package com.genspark.quizapp;
 
+import com.genspark.quizapp.Entity.User;
+import com.genspark.quizapp.Repository.CompletedQuizRepository;
+import com.genspark.quizapp.Repository.QuizRepository;
+import com.genspark.quizapp.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class QuizappApplication {
 
+@SpringBootApplication
+public class QuizappApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private QuizRepository quizRepository;
+	@Autowired
+	private CompletedQuizRepository completedQuizRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(QuizappApplication.class, args);
+	}
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.save(new User(1, "demo", "demo@demo.com", "{noop}password"));
 	}
 
 }
