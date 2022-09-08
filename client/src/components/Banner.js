07942a413c0f4fa06d8d07d9ca9370ea45397115
Sean Gates <seanx63@gmail.com>
2022-09-08 10:18:09 -0700

    Fix typos

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -6,7 +6,7 @@ export default function Banner() {
         <div class="container h-100">
             <div class="row h-100 align-items-center">
               <div class="col-12 text-center">
-                  <h1 class="banner_text fw-light" >Surprice Quiz!</h1>
+                  <h1 class="banner_text fw-light" >Surprise Quiz!</h1>
               </div>
             </div>
         </div>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -15,15 +15,15 @@ export default function Login() {
         <div class="container h-100 login_form">
             <div class="row h-100 align-items-center">
               <div class="col-12 text-center">
-              {createAccount ? <h1>Create your accout</h1> : <h1>Log in to your account</h1>}
+              {createAccount ? <h1>Create your account</h1> : <h1>Log in to your account</h1>}
                 {createAccount ? <SignUp/> : <SignIn/>}
                 {createAccount ? 
                     <div>
-                        <p>Don't have an account?</p><button class="btn btn-success" onClick={()=>changeCreateAccount()}>Create one!</button>
-                    </div> : 
+                      <p>Already have an account?</p><button class="btn btn-success" onClick={()=>changeCreateAccount()}>Sign in!</button>
+                    </div> :
                     <div>
-                        <p>Already have an account?</p><button class="btn btn-success" onClick={()=>changeCreateAccount()}>Sign in!</button>
-                    </div>
+                        <p>Don't have an account?</p><button class="btn btn-success" onClick={()=>changeCreateAccount()}>Create one!</button>
+                    </div> 
                 }
               </div>
             </div>
